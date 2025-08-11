1fb61f099ddeb75b412528f8f7c999d038fa05b3
chaki8923 <aowp.contact@gmail.com>
2025-08-11 12:44:05 +0900

    feat: ホームタブ終了

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,68 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      // ニュース画像用のドメイン設定
+      {
+        protocol: 'https',
+        hostname: 'newsatcl-pctr.c.yimg.jp',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.yimg.jp',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.yahoo.co.jp',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.asahi.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.nikkei.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.sankei.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.mainichi.jp',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.kyodo.co.jp',
+        port: '',
+        pathname: '/**',
+      },
+      // その他の一般的なニュースサイト
+      {
+        protocol: 'https',
+        hostname: '*.jiji.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.reuters.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   async rewrites() {
